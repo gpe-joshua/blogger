e72bb282a016f7b4d5f87f02a77e93b85378d3c1
gpe-joshua <59639037+gpe-joshua@users.noreply.github.com>
2020-09-19 11:55:13 +0900

    Delete current-date.js

--- current-date.js
+++
@@ -1,9 +0,0 @@
-if (document.getElementById('cur-ymd') && document.getElementById('cur-day') && document.getElementById('cur-time')) {
-  let curDate = new Date();
-  let weekName = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-
-  document.getElementById('cur-ymd').textContent =
-    curDate.getFullYear() + '년 ' + (curDate.getMonth() + 1) + '월 ' + curDate.getDate() + '일';
-  document.getElementById('cur-day').textContent = weekName[curDate.getDay()];
-  document.getElementById('cur-time').textContent = curDate.toLocaleTimeString();
-}
